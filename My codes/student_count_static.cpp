8ff0b68d65979bc6e6afd549808dd360e11e9c7b
Narayan Aryal <narayanaryal927@gmail.com>
2026-06-22 19:23:29 +0545

    Add files via upload

---
+++ My codes/student_count_static.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+private:
+    int rollNo; // Non-static data member
+
+public:
+    static int count; // Static data member
+
+    Student(int r = 0)
+    {
+        rollNo = r;
+        count++; // Increment count whenever an object is created
+    }
+
+    void display()
+    {
+        cout << "Roll No: " << rollNo << endl;
+    }
+};
+
+// Definition and initialization of static data member
+int Student::count = 0;
+
+int main()
+{
+    Student s1(101);
+    Student s2(102);
+    Student s3(103);
+    Student s4(104);
+
+    cout << "Student Details:" << endl;
+    s1.display();
+    s2.display();
+    s3.display();
+
+    cout << "\nTotal Students Created: " << Student::count << endl;
+
+    return 0;
+}
