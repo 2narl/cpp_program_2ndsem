e52d2a057e4762567bb715ae333f2d6599699d10
arl07-2n <2naryal@gmail.com>
2026-07-10 21:42:11 +0545

    Add

---
+++ Assinment/Assinment First/Lab_11_Static_Data_Member_And_Function.cpp
@@ -0,0 +1,6 @@
+/*Lab 11 (Static Data Member & Function): Write a class Product with name and
+serialNumber. Use a static data member counter to automatically generate a
+unique serial number for each Product object created. Write a static member
+function getTotalProducts() that returns the total number of Product objects
+created. Test your class in main() by creating multiple objects and displaying
+their serial numbers and the total count. */
