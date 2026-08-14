7f457e7ef8dd04479b7539c62161045d3717eab3
arl07-2n <2naryal@gmail.com>
2026-08-14 20:16:01 +0545

    Update qn4_Function_Overloading_Area.cpp

--- Assinment/Final Assingment/qn4_Function_Overloading_Area.cpp
+++ Assinment/Final Assingment/qn4_Function_Overloading_Area.cpp
@@ -1,20 +1,43 @@
-/*Write a program to calculate the area of a circle,
+/*Qn.4 Write a program to calculate the area of a circle,
 a rectangle, and a triangle using overloaded functions named area().*/
 
 #include <iostream>
 using namespace std;
 
+// Area of Circle
 double area(double r)
 {
-    return r * 22 / 7;
+    return 3.14159 * r * r;
 }
-
+// Area of Rectungle
 double area(double l, double b)
 {
     return l * b;
 }
-
+// Area of Triangle
 double area(double b, double h)
 {
-    return b * h * 1 / 2;
+    return 0.5 * b * h;
+}
+
+int main()
+{
+    double radius, length, width, base, height;
+
+    cout << "Enter radius of circle: ";
+    cin >> radius;
+
+    cout << "Area of circle = " << area(radius) << endl;
+
+    cout << "Enter length and width of rectangle: ";
+    cin >> length >> width;
+
+    cout << "Area of rectangle = " << area(length, width) << endl;
+
+    cout << "Enter base and height of triangle: ";
+    cin >> base >> height;
+
+    cout << "Area of triangle = " << area(base, height) << endl;
+
+    return 0;
 }
