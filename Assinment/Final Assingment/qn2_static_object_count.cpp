302a6e206ef8e57ebbfa22985a7cf53216dca51a
arl07-2n <2naryal@gmail.com>
2026-08-14 14:33:10 +0545

    Update qn2_static_object_count.cpp

--- Assinment/Final Assingment/qn2_static_object_count.cpp
+++ Assinment/Final Assingment/qn2_static_object_count.cpp
@@ -1,4 +1,14 @@
 /*Write a program to display the number of objects
 created so far, using a static data member.*/
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Student
+{
+private:
+    string name;
+    int age;
+
+public:
+};
