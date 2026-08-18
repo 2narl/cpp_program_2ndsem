ee1affa9485a00a014aab45e107a81cdecf53f15
arl07-2n <2naryal@gmail.com>
2026-08-18 14:39:40 +0545

    Create qn9_DistanceAddition.cpp

---
+++ Assinment/Final Assingment/qn9_DistanceAddition.cpp
@@ -0,0 +1,7 @@
+/*9 Write a program that overloads the '+' operator (using a friend function)
+to add two objects of a Distance class (feet and inches), and also write a
+conversion routine that converts the result into metres and centimetres
+(1 foot = 0.3048 m, 1 inch = 2.54 cm).*/
+
+#include <iostream>
+using namespace std;
