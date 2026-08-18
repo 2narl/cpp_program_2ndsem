22136c5b65504f038953e4efbbafb8da116415ac
arl07-2n <2naryal@gmail.com>
2026-08-18 18:56:42 +0545

    Create qn15_employee_manager_virtual_salary.cpp

---
+++ Assinment/Final Assingment/qn15_employee_manager_virtual_salary.cpp
@@ -0,0 +1,45 @@
+/*Write a program in C++ to define a class ‘Employee’ with a virtual function
+‘Salary’. Derive the class ‘Manager’ from the class ‘Employee’ and implement
+the salary function. assumptions.*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee
+{
+protected:
+    int id;
+    string name;
+    float basicSalary;
+
+public:
+    Employee(int i, string n, float s) : id(i), name(n), basicSalary(s) {}
+
+    virtual void Salary()
+    {
+        cout << "Employee Salary: Rs. " << basicSalary << endl;
+    }
+};
+
+class Manager : public Employee
+{
+private:
+    double bonus;
+
+public:
+    Manager(int i, string n, float s, float b)
+        : Employee(i, n, s), bonus(b) {}
+
+    // Override virtual function
+    void Salary() override
+    {
+        double totalSalary = basicSalary + bonus;
+
+        cout << "Id: " << id << endl;
+        cout << "Name: " << name << endl;
+        cout << "Basic Salary: Rs. " << basicSalary << endl;
+        cout << "Bonus: Rs. " << bonus << endl;
+        cout << "Total Salary: Rs. " << totalSalary << endl;
+    }
+};
