48755540facca600fa45415e48ee922bb07d4e92
arl07-2n <2naryal@gmail.com>
2026-08-18 16:21:49 +0545

    Update qn9_DistanceAddition.cpp

--- Assinment/Final Assingment/qn9_DistanceAddition.cpp
+++ Assinment/Final Assingment/qn9_DistanceAddition.cpp
@@ -5,3 +5,33 @@ conversion routine that converts the result into metres and centimetres
 
 #include <iostream>
 using namespace std;
+
+class Distance
+{
+private:
+    int feet;
+    float inches;
+
+public:
+    Distance(int f = 0, float i = 0.0) : feet(f), inches(i)
+    {
+        if (inches >= 12.0)
+        {
+            feet += (int)(inches / 12);
+            inches = (int)inches % 12 + (inches - (int)inches);
+        }
+    }
+    void display() const
+    {
+        cout << feet << "Feet, " << inches << " inches" << endl;
+    }
+
+    friend Distance operator+(const Distance &d1, const Distance &d2);
+
+    void tometers() const
+    {
+        double totalMeters = (feet * 0.3048) + (inches * 0.0254);
+        int meters = (int)totalMeters;
+        double centimeters = (totalMeters - meters) * 100;
+    }
+};
