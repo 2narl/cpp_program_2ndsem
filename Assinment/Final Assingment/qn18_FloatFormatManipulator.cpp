30db35ec36414cdc4fa92c2dc43ff641575f233b
arl07-2n <2naryal@gmail.com>
2026-08-19 17:30:31 +0545

    create

---
+++ Assinment/Final Assingment/qn18_FloatFormatManipulator.cpp
@@ -0,0 +1,34 @@
+/*Design a single manipulator format to provide the following output specifications for printing float values:
+    (i) 10 column width
+    (ii) Right Justified
+    (iii) Two-digit precision
+    (iv) Filling of unused places with *
+    (v) Trailing zeroes shown*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+// User-defined manipulator
+ostream &floatFormat(ostream &out)
+{
+    out << right
+        << setfill('*')
+        << setw(10)
+        << fixed
+        << setprecision(2);
+
+    return out;
+}
+
+int main()
+{
+    float num;
+
+    cout << "Enter a floating-point number: ";
+    cin >> num;
+
+    cout << "Formatted value: " << floatFormat << num << endl;
+
+    return 0;
+}
---
+++ Assinment/Final Assingment/qn22_SavingsBankAccount.cpp
@@ -0,0 +1,5 @@
+/*Qn.22 22.	Write a C++ program to create an Account class
+to manage Savings Bank Account. Define constructor for this
+class. Define a method to display the account details including
+current balance in the account. Also define and implement a
+friend function to calculate simple interest. Make necessary assumptions required.*/
